e6c3d2dae423b08032b9f9e2bed8b5c732562798
7thCoder <nuibima@gmail.com>
2023-09-23 23:30:21 +0100

    Added new files

---
+++ Java exercises/exercise6/src/main/java/com/example/Main.java
@@ -0,0 +1,33 @@
+/*
+* program fro printing out a pyramid in Java
+*/
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(towerBuilder(4));
+    }
+    public static String[] towerBuilder(int nFloors)
+    {
+        String[] res = new String[nFloors];
+        String[] resu = new String[nFloors];
+        StringBuilder str = new StringBuilder();
+        int i = 0;
+        int j = 0;
+        for(i = 0; i<nFloors; i++){
+        while(j <= i){
+            str.append("*");
+            j++;
+        }
+        res[i] = str.toString();
+        }
+        i = 0;
+        for(String a:res){
+           resu[i] = a;
+           i++;
+        }
+
+        return resu;
+        
+    }
+}
